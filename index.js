428e7f69c4909f4fdf516e76d2485a979226be3d
Asif-ddu <asifaddubb@gmail.com>
2022-04-04 17:49:55 -0500

    Added new files

---
+++ index.js
@@ -0,0 +1,11 @@
+function nowFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+  var a=2;
+  let b=3;
+  const c=4;
+  d = a+b+c;
+function thenFunction() {
+    document.getElementById("demo1").innerHTML = "Hello!";
+    document.getElementById("demo2").innerHTML = "Nope!";
+}
